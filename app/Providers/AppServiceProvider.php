0af5b9dca91657e751fc6a32f15bbcd2ff6366a0
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-14 15:11:46 -0300

    initial

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->setupLogViewer();
+    }
+
+    private function setupLogViewer()
+    {
+        LogViewer::auth(fn($request) => $request->user()?->is_admin);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn() => view('welcome'));
+
+if(config('app.env') === 'local') {
+    Route::get('/login/{id}', fn ($id) => Auth::loginUsingId($id));
+}
